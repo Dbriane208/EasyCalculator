a46dc276ce26a544386173b3b99dd5e91b392ce9
Daniel Brian <db9755949@gmail.com>
2023-05-20 23:03:57 +0300

    removed Brian and added Dekut kenya

--- app/src/main/java/daniel/brian/easycalculator/MainActivity.kt
+++ app/src/main/java/daniel/brian/easycalculator/MainActivity.kt
@@ -2,10 +2,18 @@ package daniel.brian.easycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        }
+    fun OnDigit(view : View){
+        Toast.makeText(view.context,"Button Clicked",Toast.LENGTH_SHORT).show()
+
     }
 }
